9a1093171534c9958c478a42b021d4a34e2b379f
Aditya Sharma <adityapandit264@gmail.com>
2025-07-31 12:46:19 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,10 +25,13 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center">
-            <Link
+           <Link
               to="/"
               className="text-3xl font-bold text-blue-600 cursor-pointer"
-              onClick={handleNavClick}
+              onClick={() => {
+                handleNavClick();
+                window.scrollTo({ top: 0, behavior: "smooth" });
+              }}
             >
               Lokaci Pro
             </Link>
